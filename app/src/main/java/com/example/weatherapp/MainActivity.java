87c10c948f8e391d0980e04a08b152629cd6b5e2
zhegmaks <zhegmaks@gmail.com>
2021-10-30 18:15:01 +0300

    0.0.1.8a

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,25 +1,16 @@
 package com.example.weatherapp;
 
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity;
-=======
-import static java.lang.Thread.sleep;
-
 import androidx.appcompat.app.AppCompatActivity;
 
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
-<<<<<<< HEAD
-import com.google.gson.Gson;
-=======
 
 import com.google.gson.Gson;
 
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -30,11 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button getWeather;
     TextView weatherText;
     EditText inputText;
-<<<<<<< HEAD
     String oldValue;
-=======
     final String TOKEN = ""; // токен API openweathermap.org
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,33 +33,25 @@ public class MainActivity extends AppCompatActivity {
         weatherText = findViewById(R.id.weather); // Текст, который изменяется для вывода погоды
         inputText = findViewById(R.id.editcity); // Текст из поля
 
-<<<<<<< HEAD
         final String TOKEN_API = getString(R.string.token); // токен API openweathermap.org
 
-=======
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
         Runnable json_parse = new Runnable() {
             @Override
             public void run() {
                 try {
                     String inputString = inputText.getText().toString();
-<<<<<<< HEAD
 
-                    if(inputString.equals(oldValue)) { // против клацания
+                    if (inputString.equals(oldValue)) { // против клацания
                         return;
                     }
 
                     String url = String.format("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", inputString, TOKEN_API); // в аргументах указываем введенный город и токен
-=======
-                    String url = String.format("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", inputString, TOKEN); // в аргументах указываем введенный город и токен
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
                     URL obj = new URL(url);
                     HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
                     connection.setRequestMethod("POST");
                     BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                     String inputLine;
                     StringBuffer response = new StringBuffer();
-<<<<<<< HEAD
 
                     while ((inputLine = in.readLine()) != null) {
                         response.append(inputLine);
@@ -89,16 +69,13 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         String cloudsText = "";
                         int clouds = Integer.valueOf(fields.clouds.getClouds());
-                        if(clouds < 10) {
+                        if (clouds < 10) {
                             cloudsText = "Безоблачно";
-                        }
-                        else if(clouds < 40 ) {
+                        } else if (clouds < 40) {
                             cloudsText = "Слабая облачность";
-                        }
-                        else if(clouds < 70) {
+                        } else if (clouds < 70) {
                             cloudsText = "Средняя облачность";
-                        }
-                        else if(clouds > 70) {
+                        } else if (clouds > 70) {
                             cloudsText = "Пасмурно";
                         }
                         new sendMessage("Погода в городе " + inputString + " " + String.format("%.0f", temp) + "°C" + "\nВлажность воздуха: " + fields.main.getHumidity() + "%\n" + cloudsText);
@@ -111,43 +88,6 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-=======
-                    while ((inputLine = in.readLine()) != null) {
-                        response.append(inputLine);
-                    }
-                    in.close();
-                    Gson gson = new Gson();
-                    customInformer custom = gson.fromJson(String.valueOf(response), customInformer.class);
-                    System.out.println(response);
-                    Float temp = Float.valueOf(custom.main.temp) - 273.15f; // Кельвины -> Цельсии
-                    if (inputString.isEmpty()) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                weatherText.setText("Введи город!");
-                            }
-                        });
-                        // sleep(100);
-                    } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                weatherText.setText("Погода в городе " + inputString + " " + String.format("%.0f", temp) + "°C");
-                            }
-                        });
-                    }
-                } catch (IOException | NullPointerException e) {
-                    e.printStackTrace();
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            weatherText.setText("Возможно, введенный город неверный");
-                        }
-                    });
-                }
-            }
-        };
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
         getWeather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -156,7 +96,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-<<<<<<< HEAD
 
     public class getFields {
         private fields main;
@@ -183,6 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
     public class sendMessage {
         private String text;
+
         sendMessage(String text) {
             this.text = text;
             runOnUiThread(new Runnable() {
@@ -193,14 +133,5 @@ public class MainActivity extends AppCompatActivity {
             });
         }
     }
-=======
-    public class customInformer {
-        public A main;
-
-    }
-    public class A {
-        public String temp;
-    }
-
->>>>>>> e2fd5e7f5c4dd82976a5c530666d6c0cbe8e6b9c
 }
+
